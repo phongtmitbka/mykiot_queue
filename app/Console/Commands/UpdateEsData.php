15390f7678f7af4247c22a068559937c4f0fbf88
PhongTM <phong.tn@citigo.com.vn>
2020-05-20 15:04:39 +0700

    Start project

---
+++ app/Console/Commands/UpdateEsData.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use App\Jobs\UpdateEsDataJob;
+use Elasticsearch\ClientBuilder;
+
+class UpdateEsData extends Command
+{
+    const CHUNK_COUNT = 5000;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:es-data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $esHost = [
+            env('ELASTICSEARCH_HOST', 'elasticsearch') . ':9200'
+        ];
+        $client = ClientBuilder::create()->setHosts($esHost)->build();
+        $indexName = 'mk_products';
+        if (!$client->indices()->exists(['index' => $indexName])) {
+            $params = [
+                'index' => $indexName,
+                'body' => [
+                    "settings" => [
+                        "analysis" => [
+                            "filter" => [
+                                "whitespace_replace" => [
+                                    "type" => "pattern_replace",
+                                    "pattern" => " ",
+                                    "replacement" => "_"
+                                ]
+                            ],
+                            "analyzer" => [
+                                "analyzer_keywords" => [
+                                    "type" => "custom",
+                                    "tokenizer" => "keyword",
+                                    "filter" => [
+                                        "asciifolding",
+                                        "whitespace_replace",
+                                        "lowercase"
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ],
+                    "mappings" => [
+                        "properties" => [
+                            "attributes" => [
+                                "type" => "nested",
+                                "properties" => [
+                                    "attributeName" => [
+                                        "type" => "text",
+                                        "fields" => [
+                                            "keyword" => [
+                                                "type" => "text",
+                                                "fielddata" => true,
+                                                "analyzer" => "keyword",
+                                                "search_analyzer" => "analyzer_keywords"
+                                            ]
+                                        ],
+                                    ],
+                                    "attributeValue" => [
+                                        "type" => "text",
+                                        "fields" => [
+                                            "keyword" => [
+                                                "type" => "text",
+                                                "fielddata" => true,
+                                                "analyzer" => "keyword",
+                                                "search_analyzer" => "analyzer_keywords"
+                                            ]
+                                        ],
+                                    ]
+                                ]
+                            ],
+                            "inventories" => [
+                                'type' => 'nested'
+                            ],
+                            "priceBooks" => [
+                                'type' => 'nested'
+                            ]
+                        ]
+                    ]
+                ]
+            ];
+            $client->indices()->create($params);
+        }
+        \DB::table('cache_products')->chunkById(UpdateEsData::CHUNK_COUNT, function ($products) {
+            try {
+                dispatch(new UpdateEsDataJob($products));
+            } catch (\Exception $e) {
+                Log::error($e->getMessage());
+            }
+        });
+    }
+}
---
+++ app/Jobs/UpdateEsDataJob.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Jobs;
+
+use Elasticsearch\ClientBuilder;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateEsDataJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $products;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($products)
+    {
+        $this->products = $products;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $esHost = [
+            env('ELASTICSEARCH_HOST', 'elasticsearch') . ':9200'
+        ];
+        $client = ClientBuilder::create()->setHosts($esHost)->build();
+        $indexName = 'mk_products';
+        foreach ($this->products as $product) {
+            $data = json_decode($product->data, true);
+            $params = [
+                'index' => $indexName,
+                'id' => $product->refer_id,
+                'body' => $data ? $data : [],
+                'type' => '_doc'
+            ];
+
+            try {
+                $client->index($params);
+            } catch (\Exception $e) {
+                echo $e->getMessage();
+            }
+        }
+    }
+}
