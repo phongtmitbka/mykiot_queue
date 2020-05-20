85863f80d2a5d78a6c589957c6d0e7f206c34573
PhongTM <phong.tn@citigo.com.vn>
2020-05-20 18:41:45 +0700

    query by lastUpdateTime

--- app/Console/Commands/UpdateEsData.php
+++ app/Console/Commands/UpdateEsData.php
@@ -2,10 +2,12 @@
 
 namespace App\Console\Commands;
 
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 use App\Jobs\UpdateEsDataJob;
 use Elasticsearch\ClientBuilder;
+use Illuminate\Support\Facades\Redis;
 
 class UpdateEsData extends Command
 {
@@ -113,10 +115,15 @@ class UpdateEsData extends Command
             ];
             $client->indices()->create($params);
         }
-        \DB::table('cache_products')->where('updated_at', '>', ':sql_last_value')
-            ->chunkById(UpdateEsData::CHUNK_COUNT, function ($products) {
+        $lastUpdateTime = Redis::get('LAST_UPDATE_TIME_ES_DATA') ?? '1970:01:01';
+        Log::debug($lastUpdateTime);
+        $currentTime = Carbon::now();
+        \DB::table('cache_products')
+            ->where('updated_at', '>=', $lastUpdateTime)
+            ->chunkById(UpdateEsData::CHUNK_COUNT, function ($products) use ($currentTime) {
             try {
                 dispatch(new UpdateEsDataJob($products));
+                Redis::set('LAST_UPDATE_TIME_ES_DATA', $currentTime);
             } catch (\Exception $e) {
                 Log::error($e->getMessage());
             }
