33a624399bd83f7586e26afb7c29dc6eb4823a77
PhongTM <phong.tn@citigo.com.vn>
2020-06-02 16:06:22 +0700

    Update redis queue

--- config/database.php
+++ config/database.php
@@ -134,6 +134,14 @@ return [
             'database' => env('REDIS_DB', '0'),
         ],
 
+        'mk_queue' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_DB', '0'),
+        ],
+
         'cache' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
